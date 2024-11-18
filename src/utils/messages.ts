6c413a66d72960c8c34e00200e7ac93bf30bebe8
Knot <akthakorn@gmail.com>
2024-11-18 22:40:38 +0700

    feat: add messages

---
+++ src/utils/messages.ts
@@ -0,0 +1,12 @@
+export const MESSAGE = {
+  TH: {
+    ERROR: {
+      INVALID_DATA: 'ข้อมูลไม่ถูกต้อง',
+    },
+  },
+  EN: {
+    ERROR: {
+      INVALID_DATA: 'Invalid data.',
+    },
+  },
+};
