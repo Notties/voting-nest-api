0594f79e751a2f6f4b898675a4f137edcaf052ad
Knot <akthakorn@gmail.com>
2024-11-18 22:39:54 +0700

    feat: add swagger ui

---
+++ src/swagger/swagger.config.ts
@@ -0,0 +1,11 @@
+export interface SwaggerConfig {
+  title: string;
+  description: string;
+  version: string;
+}
+
+export const SWAGGER_CONFIG: SwaggerConfig = {
+  title: 'VOTING-NEST-API',
+  description: 'the description of the API',
+  version: '1.0.0',
+};
---
+++ src/swagger/swagger.ts
@@ -0,0 +1,17 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { SWAGGER_CONFIG } from './swagger.config';
+import { HealthModule } from 'src/health/health.module';
+
+export function createDocument(app: INestApplication): OpenAPIObject {
+  const options = new DocumentBuilder()
+    .setTitle(SWAGGER_CONFIG.title)
+    .setDescription(SWAGGER_CONFIG.description)
+    .setVersion(SWAGGER_CONFIG.version)
+    // .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .build();
+
+  return SwaggerModule.createDocument(app, options, {
+    include: [HealthModule],
+  });
+}
