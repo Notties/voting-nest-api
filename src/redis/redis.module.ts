2ff47e150331d3788c3b8d53c1b44fdb31678437
Knot <akthakorn@gmail.com>
2024-11-20 16:44:46 +0700

    chore: add connection logger-IORedis

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,22 +1,35 @@
-import { Global, Module, OnApplicationShutdown } from '@nestjs/common';
+import { Global, Logger, Module, OnApplicationShutdown } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
-import { Redis } from 'ioredis';
+import IORedis, { Redis } from 'ioredis';
 import { EnvModule } from '../env/env.module';
 import { EnvService } from '../env/env.service';
 import { RedisService } from './redis.service';
 import { REDIS_CLIENT } from './redis.constants';
-
+import { ConfigModule } from '@nestjs/config';
 @Global()
 @Module({
-  imports: [EnvModule],
+  imports: [EnvModule, ConfigModule],
   providers: [
     {
       provide: REDIS_CLIENT,
       useFactory: async (env: EnvService) => {
-        return new Redis({
+        const logger = new Logger('RedisModule');
+        const connectionOptions = {
           host: env.REDIS_CONFIG.host,
           port: env.REDIS_CONFIG.port,
+        };
+        const client: Redis = new IORedis(connectionOptions);
+        client.on('error', (err) => {
+          logger.error('Redis Client Error: ', err);
         });
+        client.on('connect', () => {
+          logger.log(
+            `Connected to redis on ${client.options.host}:${client.options.port}`,
+          );
+        });
+        return {
+          connectionOptions,
+        };
       },
       inject: [EnvService],
     },
